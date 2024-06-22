16c29013f9b1198bd2bdc37347b75558ba0fc502
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-06-22 15:30:36 +0200

    added homepage button in /blog

--- app/blog/page.js
+++ app/blog/page.js
@@ -3,6 +3,10 @@
 import { personalData } from "@/utils/data/personal-data";
 import { Card, CardContainer } from "../components/helper/Card";
 import BlogCard from "../components/homepage/blog/blog-card";
+import { FaArrowLeft } from "react-icons/fa";
+import Link from "next/link";
+
+
 async function getBlogs() {
   const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
 
@@ -45,6 +49,16 @@ async function page() {
             <Card identifier={"blogs-cards-1"} colortype="red" imgsrc="/image/coming soon.jpg" title="Blogs Coming Soon" description="I'm currently working on creating blogs to document my learnings and share knowledge and experience about embedded software, hardware, and more. Stay tuned for insightful articles and updates!" tags={["Coming Soon", "work in progress", "under construction"]} />
           </CardContainer>
       }
+      <div className="flex justify-center  mt-5 lg:mt-12">
+        <Link
+          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+          role="button"
+          href="/"
+        >
+          <FaArrowLeft size={16} />
+          <span>Home page</span>
+        </Link>
+      </div>
     </div>
   );
 };
