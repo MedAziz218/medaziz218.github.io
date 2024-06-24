dfc3a434acabd506b69680d5e8cad55718d1e1cf
Mohamed Aziz Lahdheri <118969900+MedAziz218@users.noreply.github.com>
2024-06-24 12:09:14 +0200

    added title to herosection code added .js extention to projects titles

--- app/components/homepage/hero-section/index.jsx
+++ app/components/homepage/hero-section/index.jsx
@@ -91,13 +91,19 @@ function HeroSection() {
             <div class="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
             <div class="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
           </div>
-          <div class="px-4 lg:px-8 py-5">
-            <div class="flex flex-row space-x-2">
-              <div class="h-3 w-3 rounded-full bg-red-400"></div>
-              <div class="h-3 w-3 rounded-full bg-orange-400"></div>
-              <div class="h-3 w-3 rounded-full bg-green-200"></div>
+          <div class="px-4 lg:px-8 py-2">
+            <div class="flex flex-row items-center justify-between">
+              <div class="flex flex-row space-x-2">
+                <div class="h-3 w-3 rounded-full bg-red-400"></div>
+                <div class="h-3 w-3 rounded-full bg-orange-400"></div>
+                <div class="h-3 w-3 rounded-full bg-green-200"></div>
+              </div>
+              <p class="text-center text-[#16f2b3] text-base lg:text-xl flex-grow flex justify-center">
+              profile.cpp
+              </p>
             </div>
           </div>
+
           <div class="overflow-hidden border-t-[2px] border-indigo-900 px-4 lg:px-8 py-4 lg:py-4">
             <code class="font-mono text-xs md:text-sm lg:text-base">
               <div>
@@ -108,8 +114,8 @@ function HeroSection() {
                 <span class="text-gray-400">&gt;</span>
               </div>
               <div>
-              <span class="text-gray-400">#</span>
-              <span class="text-pink-500 ">include </span>
+                <span class="text-gray-400">#</span>
+                <span class="text-pink-500 ">include </span>
                 <span class="text-gray-400">&lt;</span>
                 <span class="text-amber-300">vector</span>
                 <span class="text-gray-400">&gt;</span>
